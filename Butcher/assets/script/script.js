7161a3e314a4ddc5d62d6b43b3067f21904945b3
Kiran lal J <kiranlalj@Kiran-lals-notbook.local>
2023-10-30 10:01:33 +0530

    butcher home page

---
+++ Butcher/assets/script/script.js
@@ -0,0 +1,39 @@
+// burger menu
+
+function navmenu() {
+  var x = document.getElementById("navMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+// carousal
+
+var swiper = new Swiper(".swiper", {
+  observer: true,
+  observeParents: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoint: {
+    1280: {
+      slidesPerView: 3,
+    },
+    769:{
+      slidesPerView: 2,
+    },
+    600:{
+      slidesPerView: 1,
+    }
+  }
+});
+swiper.update();
