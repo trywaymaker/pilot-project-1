ca69b9c5c412d83cafe2820476829046edffcadf
Kiran lal J <kiranlalj@Kiran-lals-notbook.local>
2023-10-31 11:46:37 +0530

    butcher home page

--- Butcher/assets/script/script.js
+++ Butcher/assets/script/script.js
@@ -1,7 +1,7 @@
 // burger menu
 
-function navmenu() {
-  var x = document.getElementById("navMenu");
+function navMenu() {
+  var x = document.getElementById("menuitems");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
@@ -11,29 +11,57 @@ function navmenu() {
 
 // carousal
 
-var swiper = new Swiper(".swiper", {
-  observer: true,
-  observeParents: true,
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  centerSlide: true,
-  fade: true,
+// var swiper = new Swiper(".swiper", {
+//   observer: true,
+//   observeParents: true,
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   loop: true,
+//   centerSlide: true,
+//   fade: true,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   breakpoint: {
+//     1280: {
+//       slidesPerView: 3,
+//     },
+//     769:{
+//       slidesPerView: 2,
+//     },
+//     600:{
+//       slidesPerView: 1,
+//     }
+//   }
+// });
+// swiper.update();
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
   grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoint: {
-    1280: {
-      slidesPerView: 3,
-    },
-    769:{
-      slidesPerView: 2,
-    },
-    600:{
-      slidesPerView: 1,
-    }
-  }
-});
-swiper.update();
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
