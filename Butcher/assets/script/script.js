dee5b60eb530a9ff1807cc89d3e9c7f7083733cc
Kiran lal J <kiranlalj@192.168.1.7>
2023-11-02 20:18:38 +0530

    butcher home page

--- Butcher/assets/script/script.js
+++ Butcher/assets/script/script.js
@@ -9,6 +9,8 @@ function navMenu() {
   }
 };
 
+
+
 // carousal
 
 // var swiper = new Swiper(".swiper", {
@@ -37,31 +39,3 @@ function navMenu() {
 //   }
 // });
 // swiper.update();
-
-
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  centeredSlides: false,
-  slidesPerGroupSkip: 1,
-  grabCursor: true,
-  keyboard: {
-    enabled: true,
-  },
-  breakpoints: {
-    769: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-})
