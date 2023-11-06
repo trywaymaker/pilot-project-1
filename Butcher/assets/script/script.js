d1eafab47167ccf03b0e33b7078bd82318560971
Kiran lal J <kiranlalj@Kiran-lals-notbook.local>
2023-11-06 14:58:10 +0530

    butcher home page

--- Butcher/assets/script/script.js
+++ Butcher/assets/script/script.js
@@ -48,40 +48,15 @@ swiper.update();
 
 // animation
 
-// $(document).ready(function(){
-//   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-//   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//       return new bootstrap.Tooltip(tooltipTriggerEl)
-//   });
+$(document).ready(function(){
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
 
-//   $('#jingle').click(function(){
-//       $(this).toggleClass("expand");
-//       tooltipList.forEach((el) => {
-//           el.hide();
-//       });
-//   });
-
-//   function elementScrolled(elem) {
-//       var docViewTop = $(window).scrollTop();
-//       var docViewBottom = docViewTop + $(window).height();
-//       var elemTop = $(elem).offset().top;
-//       return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-//   }
-
-//   $(window).scroll(function(){
-//       if(elementScrolled('.aboutus')) {
-//           $('.aboutus').addClass('visible');
-//       }
-//       if(elementScrolled('.community')) {
-//           $('.community').addClass('visible');
-//       }
-//       $('.servicebrief .servicebrief_card').each(function(){
-//           if(elementScrolled($(this))) {
-//               $(this).addClass('visible');
-//           }
-//       });
-//       if(elementScrolled('footer')) {
-//           $('footer').addClass('visible');
-//       }
-//   });
-// });
+  $('#jingle').click(function(){
+      $(this).toggleClass("expand");
+      tooltipList.forEach((el) => {
+          el.hide();
+      });
+  })};
